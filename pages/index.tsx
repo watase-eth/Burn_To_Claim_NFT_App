020c9d5b2acf26214faaa7326c8ef569b8381214
watase-eth <seankwatase@gmail.com>
2023-05-22 08:17:12 -1000

    Initial Commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,52 +1,39 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectWallet, Web3Button, useAddress, useContract } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
+import { BURNTOCLAIM_NFT_ADDRESS, BURN_NFT_ADDRESS } from "../constants/addresses";
 
 const Home: NextPage = () => {
+  const address = useAddress();
+
+  const { contract: burnCollection } = useContract(BURN_NFT_ADDRESS);
+  const { contract: burnToClaimCollection } = useContract(BURNTOCLAIM_NFT_ADDRESS);
+
+  async function mintBurnToClaimNFT() {
+    const hasApproval = await burnCollection?.call(
+      "isApprovedForAll",
+      [address,
+      BURNTOCLAIM_NFT_ADDRESS]
+    );
+
+    if(!hasApproval) {
+      const tx = await burnCollection?.call(
+        "setApprovalForAll",
+        [BURNTOCLAIM_NFT_ADDRESS, true]
+      );
+    }
+
+    const claimTx = await burnToClaimCollection?.call("claim", [address, 1]);
+  }
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="http://thirdweb.com/">thirdweb</a>!
-        </h1>
-
-        <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.tsx</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.tsx</code> file!
-        </p>
-
-        <div className={styles.connect}>
-          <ConnectWallet />
-        </div>
-
-        <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
-          >
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
+        <ConnectWallet/>
+        <Web3Button
+          contractAddress={BURNTOCLAIM_NFT_ADDRESS}
+          action={() => mintBurnToClaimNFT()}
+        >Claim</Web3Button>
       </main>
     </div>
   );
